d9e871f88cc29af2d21a1aa8ecfa857a9ae8f7a5
houy <hywmickey@qq.com>
2021-12-19 23:36:34 +0800

    update
    update

--- js/newtab.js
+++ js/newtab.js
@@ -1,103 +1,86 @@
+bookMarkList = [
+	[
+		["Mooc", "https://www.icourse163.org/"],
+		["bilibili", "https://space.bilibili.com/456882810/favlist?fid=963174610&ftype=create"],
+		["Linux Man", "https://man7.org/linux/man-pages/index.html"],
+		["网页配色", "http://tool.c7sky.com/webcolor/#character_3"],
+		["CSDN", "https://blog.csdn.net/nav/watchers"],
+		["李文周的博客", "https://www.liwenzhou.com/"],
+		["地鼠文档", "http://wen.topgoer.com/"],
+		["Go指南", "https://tour.go-zh.org/"],
+		["touzid", "https://www.touzid.com/"],
+		["w3cschool", "https://www.w3cschool.cn/tutorial/"],
+		["C语言中文网", "http://c.biancheng.net/"],
+		["书栈网", "https://www.bookstack.cn/"],
+		["菜鸟", "https://www.runoob.com/"],
+		["divcss5", "http://www.divcss5.com/"],
+		["leetcode", "https://leetcode.com/"],
+		["Linux From Scratch", "https://www.linuxfromscratch.org/lfs/read.html"],
+		["bstTrends", "https://www.federalreserve.gov/monetarypolicy/bst_recenttrends.htm"],
+		["spglobal", "https://www.spglobal.com/"],
+		["berkshire", "https://www.cnbc.com/berkshire-hathaway-portfolio/"],
+		["berkshire", "https://stockzoa.com/fund/berkshire-hathaway-inc/"],
+		["voxchina", "http://voxchina.org/"],
+		["MBA智库百科", "https://www.mbalib.com/"],
+		["爱课程", "https://www.icourses.cn/"],
+		["linux265", "https://linux265.com/"],
+		["搜榜", "https://www.bjsoubang.com/"]
+	],
+	[
+		["dedao", "https://www.dedao.cn/"],
+		["trello", "https://trello.com/"],
+		["topbook", "https://topbook.cc/"],
+		["少数派", "https://sspai.com/"],
+		["Anki", "https://ankiuser.net/study/"],
+		["有知有行", "https://youzhiyouxing.cn/materials"],
+		["日历", "https://www.baidu.com/s?wd=日历"],
+		["音频转文字", "https://app.xunjiepdf.com/voice2text"],
+		["讯飞快读", "https://www.ffkuaidu.com/"],
+		["vocabulary", "https://www.vocabulary.com/"],
+		["全历史", "https://www.allhistory.com/"],
+		["json", "https://www.json.cn/"],
+		["statcounter", "http://gsa.statcounter.com/"],
+		["aliWebmin", "http://47.94.137.24:10000/"],
+		["COVID", "https://voice.baidu.com/act/newpneumonia/newpneumonia/"],
+		["图形计算器", "https://www.desmos.com/calculator?lang=zh-CN"],
+		["github", "https://hub.fastgit.org/"],
+		["GitHub中文社区", "https://www.githubs.cn/"],
+		["GitOpt", "./html/gitopt.html"]
+	],
+	[
+		["Algorithms", "https://www.cs.usfca.edu/~galles/visualization/Algorithms.html"],
+		["VisuAlgo", "https://visualgo.net/zh/"],
+		["algorithm", "https://algorithm-visualizer.org/"],
+		["redisTry", "https://try.redis.io/"],
+		["EF", "https://www.ef.com/wwen/english-resources/"]
+	],
+	[
+		["便签", "https://yun.smartisan.com/#/notes"],
+		["笔记", "https://note.youdao.com"],
+		["新生", "https://b.xinshengdaxue.com/index.html"],
+		["枯燥", "./html/text.html"]
+	],
+	[
+		["早时光书屋", "https://nt-registrar.zuoyebang.cc/static/fe-store-mis/#/store/store-list"],
+		["git", "https://git.afpai.com/"],
+		["监控平台", "http://op.zuoyebang.cc/static/odin/#/monitor/graph"],
+		["OA", "http://oa.zuoyebang.cc"],
+		["HR", "http://hr.zuoyebang.cc"]
+	]
+];
 
-
-//setInterval(function(){timeDown();}, 1000);
-// loadXMLDoc();
-function timeDown() {
-	var strTop = document.querySelector("body > div.top");
-	//console.log(strTop.innerHTML );
-
-	// getFullYear, getDate(), getMonth()  getHours() , getMinutes(), getSeconds()	
-
-	var objDate = new Date();
-	var strToday = objDate.getFullYear() + '-' + (objDate.getMonth() +1)+ '-' + objDate.getDate()
-	// 今天中午12点
-	var today12 = new Date(strToday + ' 12:00:00');
-	var today18 = new Date(strToday + ' 18:00:00');
-	var today20 = new Date(strToday + ' 20:00:00');
-
-
-	var objDiff12 = getFormateDiffTime(objDate , today12);
-	var objDiff18 = getFormateDiffTime(objDate , today18);
-	var objDiff20 = getFormateDiffTime(objDate , today20);
-
-	var arrTimeDiffList = new Array(objDiff12, objDiff18, objDiff20);
- 	
- 	var strNowTime = [objDate.getHours(),objDate.getMinutes(), objDate.getSeconds()].join(':');
-		strNowTime = '<span>'+strNowTime+'</span>';
-
- 	var arrTimeDiffHtml = [strNowTime];
- 	for (var i = 0; i < arrTimeDiffList.length; i++) {
- 		objTimeDiff = arrTimeDiffList[i];
-
- 		strHtml = '';
- 		if(objTimeDiff.sign > 0) {
- 			//strHtml = 
- 			strHtml = '<span>'+objTimeDiff.timeDiff+'</span>';
- 		} else {
- 			strHtml = '<span class="timeout">'+objTimeDiff.timeDiff+'</span>';
- 		}
-
- 		
- 		arrTimeDiffHtml.push(strHtml);
- 	};
-
-	strTop.innerHTML = arrTimeDiffHtml.join('');
-}
-
-
-function getFormateDiffTime(intTime1, intTime2) {
-	var allDiffSeconds = parseInt((intTime2 - intTime1) / 1000);
-	var intSign = 1;
-	if(intTime1 > intTime2) {
- 		allDiffSeconds = parseInt((intTime1 - intTime2) / 1000);
- 		intSign = -1;
+blockMarkObj = document.createDocumentFragment();
+for (i in bookMarkList) {
+	itemObj = document.createElement("div");
+	itemObj.setAttribute("class","items");
+	for (k in bookMarkList[i]) {
+		aObj = document.createElement("a");
+		aObj.text = bookMarkList[i][k][0];
+		aObj.href=bookMarkList[i][k][1];
+		pObj = document.createElement("p");
+		pObj.appendChild(aObj);
+		itemObj.appendChild(pObj);
 	}
-
-	var intHours = Math.floor(allDiffSeconds / 3600);
-	var intModulo = allDiffSeconds % (24 * 60 * 60 ); // 时 + 分 + 秒 (去掉了天)
-		intModulo = intModulo % (60 * 60); // 分 + 秒 
-	
-	var intMinutes = Math.floor(intModulo / 60);
-	var intSeconds = intModulo % 60;
-
-	var arrTime = new Array(intHours, intMinutes, intSeconds);
-	var strTime = arrTime.join(':');
-
-	return {
-		'sign' : intSign,
-		'timeDiff' : strTime
-	}
-
-	return strTime;
+	blockMarkObj.appendChild(itemObj);
 }
-
-function loadXMLDoc()
-{
-    var xmlhttp;
-    //console.log(111111);
-
-    ///*
-    if (window.XMLHttpRequest)
-    {
-        //  IE7+, Firefox, Chrome, Opera, Safari 浏览器执行代码
-        xmlhttp=new XMLHttpRequest();
-    }
-    else
-    {
-        // IE6, IE5 浏览器执行代码
-        xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-    }
-    xmlhttp.onreadystatechange=function()
-    {
-        if (xmlhttp.readyState==4 && xmlhttp.status==200)
-        {
-            //document.getElementById("myDiv").innerHTML=xmlhttp.responseText;
-
-            //console.log(xmlhttp.responseText);
-            console.log(JSON.parse(xmlhttp.responseText));
-        }
-    }
-    xmlhttp.open("GET","https://fanyi.baidu.com/pcnewcollection?req=get&dstStatus=all&order=time&direction=all&page=0&pagesize=30&gid=860772&_=1561509315056",true);
-    xmlhttp.send();
-    //*/
-}
+document.getElementById("bookmarks").appendChild(blockMarkObj);
