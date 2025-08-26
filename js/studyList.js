305c5914b2d4ae28d5ae3c5efdc271b6135b6796
v_houyongwei <v_houyongwei@baidu.com>
2025-08-26 15:46:22 +0800

    update

--- js/studyList.js
+++ js/studyList.js
@@ -90,6 +90,7 @@ function studyList() {
 			["Play with Kubernetes","https://labs.play-with-k8s.com/"],
 			["幼麟实验室","https://space.bilibili.com/567195437/video"],
 			["李文周的博客", "https://www.liwenzhou.com/"],
+			["秀才的进阶之路","https://golangstar.cn/"],
 			["地鼠文档", "http://wen.topgoer.com/"],
 			["书栈网", "https://www.bookstack.cn/"],
 			["刘丹冰作品集","https://zhuanlan.zhihu.com/p/513990972"],
