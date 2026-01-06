5bf8417263608a052cc2336b37994ec4275539a4
v_houyongwei <v_houyongwei@baidu.com>
2026-01-06 19:18:19 +0800

    update

--- js/studyList.js
+++ js/studyList.js
@@ -108,6 +108,10 @@ function studyList() {
 			["刘丹冰作品集","https://zhuanlan.zhihu.com/p/513990972"],
 			["TecMint","https://www.tecmint.com/"],
 			["Golang并发编程","https://space.bilibili.com/170957273/lists/5601350?type=season"],
+		],
+		[
+			["stablediffusio", "https://stablediffusionweb.com/"],
+			["comfy", "https://www.comfy.org/"],
 			["civitai", "https://civitai.com/"],
 			["huggingface", "https://huggingface.co/"],
 		],
